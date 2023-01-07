0ea40c31ee28db9b8d041b9dcf0c245da2f4042e
fufulouteur <manzodqb@icloud.com>
2023-01-07 09:34:08 -0500

    fini project

---
+++ main.py
@@ -0,0 +1,23 @@
+candidat = ['may', 'sami', 'carlo', 'richard', 'ashanti', 'bruno', 'laurence', 'michel', 'keitn', 'kai', 'siomon',
+            'norbert']
+
+enAttente = []
+
+enAttente.append(candidat[-1])
+print(enAttente)
+
+enAttente.append(candidat[-2])
+print(enAttente)
+
+enAttente.append(candidat[-3])
+print(enAttente)
+
+enAttente.append(candidat[-4])
+print(enAttente)
+
+enAttente.append(candidat[-5])
+print("sont en attente d'un chiot")
+print(enAttente)
+
+enAttente.sort()
+print(enAttente)
